925e8e3e0ba4e20a6b91d40832a373bd1d5ce8ad
HeGuolun <heguolun@deepin.com>
2020-06-03 17:35:46 +0800

    fix: make transparent background resolve splash screen when switch user.
    切换用户时会出现白色闪屏，为防止白色闪屏，将背景透明化处理。
    
    Log: 优化用户切换时的体验。
    Task: https://pms.uniontech.com/zentao/task-view-24457.html

--- src/widgets/fullscreenbackground.cpp
+++ src/widgets/fullscreenbackground.cpp
@@ -54,6 +54,11 @@ FullscreenBackground::FullscreenBackground(QWidget *parent)
 //    }
 #endif
 
+    QPalette pal(this->palette());
+    pal.setColor(QPalette::Background, QColor(0,0,0,0));
+    this->setAutoFillBackground(true);
+    this->setPalette(pal);
+
     m_fadeOutAni->setEasingCurve(QEasingCurve::InOutCubic);
     m_fadeOutAni->setDuration(1000);
     m_fadeOutAni->setStartValue(1.0f);
