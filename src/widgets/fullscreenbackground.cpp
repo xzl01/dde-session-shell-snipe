2a70aa6f64da374e5433772b97118b0e6ec2aab8
zhangqipeng <zhangqipeng@uniontech.com>
2020-07-09 16:32:04 +0800

    Revert "fix: 增加关机/锁屏界面窗口属性"
    This reverts commit 9749b159486c045e971c74d8bf73e7c96e4665fd.

--- src/widgets/fullscreenbackground.cpp
+++ src/widgets/fullscreenbackground.cpp
@@ -38,8 +38,6 @@
 #include <QDir>
 #include <DGuiApplicationHelper>
 
-#define SESSION_SHELL 0x00800000
-
 DGUI_USE_NAMESPACE
 
 FullscreenBackground::FullscreenBackground(QWidget *parent)
@@ -52,7 +50,7 @@ FullscreenBackground::FullscreenBackground(QWidget *parent)
 //    if(DGuiApplicationHelper::isXWindowPlatform()) {
 //        setWindowFlags(Qt::WindowStaysOnTopHint | Qt::X11BypassWindowManagerHint);
 //    } else {
-        setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::Window | static_cast<Qt::WindowFlags>(SESSION_SHELL));
+        setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint | Qt::FramelessWindowHint | Qt::Window);
 //    }
 #endif
 
