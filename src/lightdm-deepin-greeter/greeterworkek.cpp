939b945d4fa708e4a6edc901d82f35c945ce69cf
huangjiawei <huangjiawei@uniontech.com>
2020-11-18 13:26:48 +0800

    Revert "fix: 切换用户无法指纹解锁"
    This reverts commit 153b3657f79039fbfa4d0a9819c4d74796484322.
    
    Change-Id: I103a76457661cb619b3b63e2ac472237ba6f9b0e

--- src/lightdm-deepin-greeter/greeterworkek.cpp
+++ src/lightdm-deepin-greeter/greeterworkek.cpp
@@ -119,19 +119,17 @@ GreeterWorkek::GreeterWorkek(SessionBaseModel *const model, QObject *parent)
     });
 
     connect(model, &SessionBaseModel::onStatusChanged, this, [ = ](SessionBaseModel::ModeStatus status) {
-        QTimer::singleShot(100, [&](){
-            switch (status) {
-            case SessionBaseModel::ModeStatus::PasswordMode:
-                if (!m_greeter->isAuthenticated())
-                    resetLightdmAuth(m_model->currentUser(), 100, false);
-                break;
-            case SessionBaseModel::ModeStatus::UserMode:
-                checkUserOneKeyLogin();
-                break;
-            default:
-                break;
-            }
-        });
+       switch (status) {
+       case SessionBaseModel::ModeStatus::PasswordMode:
+           if (!m_greeter->isAuthenticated())
+               resetLightdmAuth(m_model->currentUser(), 100, false);
+           break;
+       case SessionBaseModel::ModeStatus::UserMode:
+           checkUserOneKeyLogin();
+           break;
+       default:
+           break;
+       }
     });
 
     connect(this, &GreeterWorkek::oneKeyLoginMatchFalse, this, &GreeterWorkek::checkUserOneKeyLogin);
