0293b75a1ce50e28e5b2b997ab1ec39f843138af
Zhang Qipeng <zhangqipeng@uniontech.com>
2021-08-12 17:40:22 +0800

    test: 增加单元测试
    增加单元测试。
    
    Log: 增加单元测试
    Change-Id: Iae986961c62cac877d98fab5100fb9dbe3e46a37

---
+++ tests/dde-lock/ut_fullscreenbackground.cpp
@@ -0,0 +1,41 @@
+#include "fullscreenbackground.h"
+#include "sessionbasemodel.h"
+
+#include <gtest/gtest.h>
+
+class UT_FullscreenBackground : public testing::Test
+{
+protected:
+    void SetUp() override;
+    void TearDown() override;
+
+    FullscreenBackground *m_background;
+    SessionBaseModel *m_model;
+};
+
+void UT_FullscreenBackground::SetUp()
+{
+    m_model = new SessionBaseModel(SessionBaseModel::LightdmType);
+    m_background = new FullscreenBackground(m_model);
+}
+
+void UT_FullscreenBackground::TearDown()
+{
+    delete m_background;
+}
+
+TEST_F(UT_FullscreenBackground, BasicTest)
+{
+    m_background->setContentVisible(true);
+    m_background->contentVisible();
+
+    // m_background->setIsBlackMode(false);
+    // m_background->setIsBlackMode(true);
+
+    // m_background->setIsHibernateMode();
+
+    m_background->enableEnterEvent(true);
+
+    m_background->updateBackground("/usr/share/backgrounds/default_background.jpg");
+    m_background->updateBlurBackground("/usr/share/backgrounds/default_background.jpg");
+}
