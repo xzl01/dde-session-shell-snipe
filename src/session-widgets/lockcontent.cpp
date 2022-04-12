8688edc1052f69c713dcb8c84a808bc578f0d9f2
dengbo <dengbo@uniontech.com>
2022-04-12 21:57:07 +0800

    fix: 系统自动锁屏时锁屏界面显示异常
    当有应用或者某个操作(任务栏右键菜单或keyring弹框)一直占用着焦点（键盘）时
    此时去隐藏整个锁屏界面，而不只是隐藏对应的锁屏相关组件，防止界面显示异常
    
    Log: 解决当存在与锁屏抢占焦点（键盘）的操作时，自动锁屏显示异常问题
    Influence: 锁屏/自动锁屏
    Bug: https://pms.uniontech.com/bug-view-123641.html
    Change-Id: I4ae1ada0273b363f9e77d75f90ad1ef94ce8eee4
    (cherry picked from commit e8dd331f2d58e0557e5afe2578315b6ca482e9ec)

--- src/session-widgets/lockcontent.cpp
+++ src/session-widgets/lockcontent.cpp
@@ -531,7 +531,8 @@ void LockContent::tryGrabKeyboard()
             .arg(5000)
             .call();
 
-        hide();
+        emit requestLockFrameHide();
+        return;
     }
 
     QTimer::singleShot(100, this, &LockContent::tryGrabKeyboard);
